4929a15e9f0f6a3655dccc9b114dd1a546736ec5
Vasko Raketic <vasx93@hotmail.com>
2024-09-16 19:51:27 +0200

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,23 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    // binary has to be done on sorted list
+    haystack.sort((a, b) => a - b);
 
+    let left = 0;
+    let right = haystack.length;
+    // right(high) is exclusive
+    // left(low) is inclusive
+    while (left < right) {
+        const middle = Math.floor(left + (right - left) / 2);
+        const val = haystack[middle];
+
+        if (val === needle) {
+            return true;
+        } else if (val > needle) {
+            right = middle;
+        } else if (val < needle) {
+            left = middle + 1;
+        }
+    }
+
+    return false;
 }
