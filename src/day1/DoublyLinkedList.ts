960a6da5d7c065e429b91728707751796dbe9e94
Vasko Raketic <vasx93@hotmail.com>
2024-09-17 13:48:16 +0200

    DLL init

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,120 @@
+class Node<T> {
+    constructor(item: T) {
+        this.value = item;
+        this.next = undefined;
+        this.prev = undefined;
+    }
+    value: T;
+    prev: Node<T> | undefined;
+    next: Node<T> | undefined;
+}
 export default class DoublyLinkedList<T> {
-    public length: number;
+    constructor() {}
+    public length = 0;
 
-    
+    head: Node<T> | undefined;
+    tail: Node<T>;
 
-    constructor() {
+    getLength() {
+        let count = 0;
+        let curr = this.head;
+
+        while (curr !== undefined) {
+            count++;
+            curr = curr?.next;
+        }
+        return count;
     }
 
-    prepend(item: T): void {
-
-}
-    insertAt(item: T, idx: number): void {
-
-}
     append(item: T): void {
+        const newNode = new Node(item);
 
-}
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let curr = this.head;
+            while (curr.next !== undefined) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+            newNode.prev = curr;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+    print(): Node<T>[] {
+        const list = [];
+
+        let curr = this.head;
+        while (curr !== undefined) {
+            list.push(curr);
+            curr = curr.next;
+        }
+        return list;
+    }
+    // prepend(item: T): void {}
+    // insertAt(item: T, idx: number): void {}
     remove(item: T): T | undefined {
+        if (this.head === undefined) return undefined;
+        else if (this.head?.value === item) {
+            this.head = this.head.next;
+            if (this.head !== undefined) {
+                this.head.prev = undefined;
+            }
+            this.length--;
+            return item;
+        } else if (this.tail.value === item) {
+            if (this.tail.prev) {
+                this.tail = this.tail.prev;
+                this.tail.next = undefined;
+                this.length--;
 
-}
+                return item;
+            }
+        }
+
+        // item is not in head or tail
+        let curr = this.head;
+
+        while (curr !== undefined) {
+            // item is B in A -> B -> C
+            if (curr.value === item) {
+                // A -> C
+                if (curr.prev) {
+                    curr.prev.next = curr.next;
+                }
+                // A <- C
+                if (curr.next) {
+                    curr.next.prev = curr.prev;
+                }
+                this.length--;
+                return item;
+            }
+        }
+        return undefined;
+    }
     get(idx: number): T | undefined {
+        if (idx >= this.length) return undefined;
 
+        let count = 0;
+        let curr = this.head;
+        while (curr !== undefined) {
+            if (count === idx) {
+                return curr.value;
+            }
+            count++;
+            curr = curr?.next;
+        }
+        return undefined;
+    }
+    // removeAt(idx: number): T | undefined {}
 }
-    removeAt(idx: number): T | undefined {
 
-}
-}
+const test = new DoublyLinkedList<number>();
+test.append(5);
+test.append(22);
+test.append(33);
+test.append(800);
+// test.print().forEach((el) => console.log(el));
+console.log(test.getLength());
+console.log(test.get(5));
