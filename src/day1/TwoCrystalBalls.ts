40b0decf8a41c3ad906e6409e463a27c5e349edc
Vasko Raketic <vasx93@hotmail.com>
2024-09-17 09:50:13 +0200

    two crystall balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // breaks: [ false, false ... false, true, true .... true]
 
+    const offset = Math.floor(Math.sqrt(breaks.length));
+    let i = offset;
+    for (; i < breaks.length; i += offset) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= offset;
+
+    for (let j = 0; j < offset && i < breaks.length; j++, i++) {
+        if (breaks[j]) {
+            return i;
+        }
+    }
+    return -1;
 }
