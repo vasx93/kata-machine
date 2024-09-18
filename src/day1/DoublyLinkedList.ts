0bea47a197325c2877961d56dabb5453010a5aab
Vasko Raketic <vasx93@hotmail.com>
2024-09-18 10:36:27 +0200

    insertAt

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -54,7 +54,75 @@ export default class DoublyLinkedList<T> {
         return list;
     }
     // prepend(item: T): void {}
-    // insertAt(item: T, idx: number): void {}
+    insertAt(item: T, idx: number): void {
+        if (idx < 0) return undefined;
+
+        const newNode = new Node(item);
+
+        if (idx === 0) {
+            if (this.head !== undefined) {
+                if (this.head.next !== undefined) {
+                    const next = this.head.next;
+                    next!.prev = newNode;
+                    newNode.next = next;
+                    newNode.prev = undefined;
+                    this.head = newNode;
+
+                    return;
+                } else {
+                    // replace head
+                    this.head = newNode;
+                    this.tail = newNode;
+                    return;
+                }
+            } else {
+                // no nodes
+                this.head = newNode;
+                this.tail = newNode;
+                this.length++;
+                return;
+            }
+        } else if (idx === this.length - 1) {
+            // replace tail
+            const prev = this.tail?.prev;
+
+            prev!.next = newNode;
+            newNode.prev = prev;
+            newNode.next = undefined;
+            this.tail = newNode;
+            return;
+        } else if (idx >= this.length && this.length > 0) {
+            // add to tail
+            this.tail!.next = newNode;
+            newNode.prev = this.tail;
+
+            this.tail = newNode;
+            this.length++;
+            return;
+        } else if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+            return;
+        }
+
+        // not head/tail or over length
+
+        let count = 0;
+        let curr = this.head;
+
+        while (curr !== undefined) {
+            if (count === idx) {
+                curr.prev!.next = newNode;
+                curr.next!.prev = newNode;
+
+                newNode.prev = curr.prev;
+                newNode.next = curr.next;
+                return;
+            }
+            count++;
+            curr = curr.next;
+        }
+    }
     remove(item: T): T | undefined {
         if (this.head === undefined) return undefined;
         else if (this.head?.value === item) {
@@ -155,13 +223,14 @@ export default class DoublyLinkedList<T> {
 }
 
 const test = new DoublyLinkedList<number>();
-test.append(5);
-test.append(22);
-test.append(33);
-test.append(800);
+// test.append(5);
+// test.append(22);
+// test.append(33);
+// test.append(800);
 console.log(test.getLength());
 // console.log(test.get(5));
 // console.log(test.removeAt(3));
-console.log(test.remove(22));
+// console.log(test.remove(22));
+console.log(test.insertAt(11, 1));
 console.log(test.getLength());
 console.log(test.print());
