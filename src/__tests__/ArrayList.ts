bd238adaf8f7b00650298412413a76326d6da28a
Vasko Raketic <vasx93@hotmail.com>
2024-09-19 18:17:42 +0200

    fix

--- src/__tests__/ArrayList.ts
+++ src/__tests__/ArrayList.ts
@@ -2,6 +2,6 @@ import ArrayList from "@code/ArrayList";
 import { test_list } from "./ListTest";
 
 test("array-list", function () {
-    const list = new ArrayList<number>(3);
+    const list = new ArrayList<number>();
     test_list(list);
 });
