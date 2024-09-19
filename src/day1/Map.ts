5a44ddc28fd158b2c1445b9d653b7d56d1257734
Vasko Raketic <vasx93@hotmail.com>
2024-09-19 18:41:29 +0200

    map

--- src/day1/Map.ts
+++ src/day1/Map.ts
@@ -1,21 +1,28 @@
-export default class Map<T extends (string | number), V> {
-    
-
-    
+export default class Map<T extends string | number, V> {
+    kv: Record<string | number, V>;
+    kvSize: number;
 
     constructor() {
+        this.kv = {};
+        this.kvSize = 0;
     }
 
     get(key: T): V | undefined {
-
-}
+        return this.kv[key];
+    }
     set(key: T, value: V): void {
-
-}
+        this.kv[key] = value;
+        this.kvSize++;
+    }
     delete(key: T): V | undefined {
+        const item = this.get(key);
+        if (!item) return undefined;
 
-}
+        delete this.kv[key];
+        this.kvSize--;
+        return item;
+    }
     size(): number {
-
-}
+        return this.kvSize;
+    }
 }
