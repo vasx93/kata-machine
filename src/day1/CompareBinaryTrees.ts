7a5b03a1e5389724311d55dbf723ba557e239a6a
Vasko Raketic <vasx93@hotmail.com>
2024-09-23 12:12:09 +0200

    b  tree compare

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,10 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    if (a === null && b === null) return true;
+    else if (!a || !b) return false;
+    else if (a.value !== b.value) return false;
 
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
