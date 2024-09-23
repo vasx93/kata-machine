4a004289cb219fbdd61b6fa7351198ddfed73882
Vasko Raketic <vasx93@hotmail.com>
2024-09-23 11:27:43 +0200

    bfs

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,14 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q: (BinaryNode<number> | null)[] = [head];
 
+    while (q.length) {
+        const node = q.shift();
+        if (!node) continue;
+
+        if (node?.value === needle) return true;
+
+        q.push(node.left);
+        q.push(node.right);
+    }
+    return false;
 }
